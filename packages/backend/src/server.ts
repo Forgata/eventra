239ebb777865cf9414c9bdb24871a4e9dca065eb
Forgata <gombwadivine@gmail.com>
2026-03-31 11:42:51 +0200

    add MongoDB connection setup and environment variable check

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app.js";
+import { connectDB } from "./infrastructure/db/connect.js";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI)
+  throw new Error("MONGODB_URI is not defined in environment variables");
+await connectDB(MONGODB_URI);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
